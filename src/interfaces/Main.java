73e663381414bd50a9ecdddc4e15eb9c61cd288a
Sherlock <wisdomnji@gmail.com>
2018-10-24 15:18:27 +0100

    Second Intial commit
    Signed-off-by: Sherlock <wisdomnji@gmail.com>

--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -18,7 +18,7 @@ public class Main {
 				"    <h5 class=\"card-title\">Card title</h5>\n" + 
 				"    <p class=\"card-text\">Some quick example text to build on the card title and make up the bulk of the card's content.</p>\n" + 
 				"    <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>\n" + 
-				"  </div>\n" + 
+				"  </div>\n" +  
 				"</div>";
 		
 		Element doc = Jsoup.parse(sampleHtml, "", Parser.xmlParser());
